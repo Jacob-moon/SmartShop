69857d26949e8af32729a3e975bf058fbaebaa1e
moon <msj7191@naver.com>
2025-06-24 21:18:50 +0900

    feat(user) : user entity 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        unique: true,
+    })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    name: string;
+
+    @Column({default: 'user'})
+    role: 'admin' | 'user';
+
+    @CreateDateColumn()
+    created_at: Date;
+}
