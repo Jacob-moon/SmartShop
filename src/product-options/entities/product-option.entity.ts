d7bdaf70790e8cec08ebeff7353e79daac954d1f
moon <msj7191@naver.com>
2025-06-25 15:17:10 +0900

    refactor(products-options) :엔티티 추가

--- src/product-options/entities/product-option.entity.ts
+++ src/product-options/entities/product-option.entity.ts
@@ -1,1 +1,19 @@
-export class ProductOption {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('product_options')
+export class ProductOption {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price_diff: number;
+
+  @Column({ nullable: true })
+  stock: number;
+
+  @Column({ default: true })
+  is_active: boolean;
+}
