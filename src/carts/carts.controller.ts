7a6a3fed0af40fc6373c2e71db8daa53b30f9db2
moon <msj7191@naver.com>
2025-06-25 21:22:50 +0900

    refactor(all): DTO 변경에 따른 controller 해당 부분 우선 제거(추후 추가)

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,16 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CartsService } from './carts.service';
-import { CreateCartDto } from './dto/create-cart.dto';
-import { UpdateCartDto } from './dto/update-cart.dto';
 
 @Controller('carts')
 export class CartsController {
   constructor(private readonly cartsService: CartsService) {}
-
-  @Post()
-  create(@Body() createCartDto: CreateCartDto) {
-    return this.cartsService.create(createCartDto);
-  }
 
   @Get()
   findAll() {
@@ -20,11 +13,6 @@ export class CartsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.cartsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
-    return this.cartsService.update(+id, updateCartDto);
   }
 
   @Delete(':id')
--- src/order_items/order_items.controller.ts
+++ src/order_items/order_items.controller.ts
@@ -1,16 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { OrderItemsService } from './order_items.service';
-import { CreateOrderItemDto } from './dto/create-order_item.dto';
-import { UpdateOrderItemDto } from './dto/update-order_item.dto';
 
 @Controller('order-items')
 export class OrderItemsController {
   constructor(private readonly orderItemsService: OrderItemsService) {}
-
-  @Post()
-  create(@Body() createOrderItemDto: CreateOrderItemDto) {
-    return this.orderItemsService.create(createOrderItemDto);
-  }
 
   @Get()
   findAll() {
@@ -20,11 +13,6 @@ export class OrderItemsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.orderItemsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderItemDto: UpdateOrderItemDto) {
-    return this.orderItemsService.update(+id, updateOrderItemDto);
   }
 
   @Delete(':id')
