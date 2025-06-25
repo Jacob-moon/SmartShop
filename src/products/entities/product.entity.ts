2ebe70067b06ed69d1b0013507e05f6e865e5c03
moon <msj7191@naver.com>
2025-06-25 15:15:19 +0900

    refactor(products) : 필드추가

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,32 @@
-export class Product {}
+import { Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column('text',{
+        nullable: true,
+    })
+    description?: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    stock: number;
+
+    @Column({
+        nullable: true,
+    })
+    image_url: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    // @ManyToOne(() => User, (user) => user.products,{onDelete: 'CSCADE'})
+    // user:User;
+}
