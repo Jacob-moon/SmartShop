b5473d8f386b0545e103a5405b2f86d6327d588e
moon <msj7191@naver.com>
2025-06-25 15:20:44 +0900

    refactor(order_item) :엔티티 추가

--- src/order_items/entities/order_item.entity.ts
+++ src/order_items/entities/order_item.entity.ts
@@ -1,1 +1,13 @@
-export class OrderItem {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('order_items')
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  price: number;
+}
