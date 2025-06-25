b62b507298656ae3d3beab4d0ddc251e98e68304
moon <msj7191@naver.com>
2025-06-25 15:19:45 +0900

    refactor(order) :엔티티 추가

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,16 @@
-export class Order {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  total_price: number;
+
+  @Column({ type: 'enum', enum: ['pending', 'paid', 'shipped'] })
+  status: 'pending' | 'paid' | 'shipped';
+
+  @CreateDateColumn()
+  created_at: Date;
+}
