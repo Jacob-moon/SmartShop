5a4fe85af6c7d85b536f24f029301df81b81401d
moon <msj7191@naver.com>
2025-06-25 20:57:32 +0900

    refactor(users,products) IsString 등 벨리데이터 추가 및 procut DTO 추가

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,12 +1,14 @@
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateAuthDto {
     @IsEmail({},{ message: "'올바은 이메일을 입력해주세요.'"})
     email:string;
 
     @IsNotEmpty({ message: '비밀번호를 입력해주세요.'})
+    @IsString()
     password: string;
 
     @IsNotEmpty({ message: '이름을 입력해주세요.'})
+    @IsString()
     name: string;
 }
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty({ message:'상품 이름을 입력해주세요' })
+    @IsString()
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsInt({ message: '가격은 숫자여야 합니다' })
+    @Min(0, { message: '가격은 0 이상이여야 합니다.' })
+    price: number;
+
+    @IsInt({ message: '재고는 숫자여야합니다' })
+    @Min(0, { message: '재고는 0 이상이여야 합니다.' })
+    stock: number;
+
+    @IsOptional()
+    @IsString()
+    image_url?: string;
+}
