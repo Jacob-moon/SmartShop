26012832ea63deaaa095fe927426bdaabea5c2bd
moon <msj7191@naver.com>
2025-06-25 15:30:31 +0900

    refactor(users,cart) :엔티티간 관계 추가

--- src/carts/entities/cart.entity.ts
+++ src/carts/entities/cart.entity.ts
@@ -1,7 +1,12 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/entities/user.entity";
+import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('carts')
 export class Cart {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @OneToOne(() => User, (user) => user.cart)
+  @JoinColumn()
+  user: User;
 }
