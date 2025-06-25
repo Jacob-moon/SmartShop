438e8a845cf7c14833d93cb5f0444a32b125546a
moon <msj7191@naver.com>
2025-06-25 21:46:18 +0900

    refacotr(app): 모듈 임포트완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users/users.controller';
 import { ProductOptionsModule } from './product-options/product-options.module';
 import { CartItemsModule } from './cart_items/cart_items.module';
 import { OrderItemsModule } from './order_items/order_items.module';
+import { AuthModule } from './auth/auth.module';
+import { CartsModule } from './carts/carts.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -22,8 +26,13 @@ import { OrderItemsModule } from './order_items/order_items.module';
     ProductOptionsModule,
     CartItemsModule,
     OrderItemsModule,
+    AuthModule,
+    CartsModule,
+    OrdersModule,
+    ProductsModule,
+    UsersModule
   ],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
