bedfdc825f583b2af672a2ad68975cf29ff1e4ca
moon <msj7191@naver.com>
2025-06-25 21:01:15 +0900

    feature(product-options) product-options  DTO 추가

--- src/product-options/dto/create-product-option.dto.ts
+++ src/product-options/dto/create-product-option.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductOptionDto {}
+import { IsBoolean, IsInt, IsNotEmpty, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateProductOptionDto {
+    @IsNotEmpty({ message: '옵션 이름을 입력해주세요' })
+    @IsString()
+    name: string;
+
+    @IsInt({ message: '가격 차이는 숫자여야 합니다' })
+    price_diff: number;
+
+    @IsOptional()
+    @IsInt({ message: '재고는 숫자여야 합니다.' })
+    @Min(0,{ message: '재고는 0이상이여야 합니다.' })
+    stock?: number;
+
+    @IsNotEmpty({ message: '활성화 여부를 입력해주세요.' })
+    @IsBoolean()
+    is_active: boolean;
+}
