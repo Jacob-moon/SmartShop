2e10e1c12487917f2ebb6bb47c52a0957454f017
moon <msj7191@naver.com>
2025-06-25 15:18:31 +0900

    refactor(cart) :엔티티 추가

--- src/cart_items/entities/cart_item.entity.ts
+++ src/cart_items/entities/cart_item.entity.ts
@@ -1,1 +1,10 @@
-export class CartItem {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('cart_items')
+export class CartItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  quantity: number;
+}
