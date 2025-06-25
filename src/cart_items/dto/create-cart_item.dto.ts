4be0c20d46d9faa83dd14c81b5f76ab976ab01c4
moon <msj7191@naver.com>
2025-06-25 21:05:23 +0900

    feature(cart-items) cart-items  DTO 추가

--- src/cart_items/dto/create-cart_item.dto.ts
+++ src/cart_items/dto/create-cart_item.dto.ts
@@ -1,1 +1,10 @@
-export class CreateCartItemDto {}
+import { IsInt, Min } from "class-validator";
+
+export class CreateCartItemDto {
+    @IsInt({ message: '상품 옵션 ID는 숫자여야 합니다.' })
+    product_options_id:number
+
+    @IsInt({ message: '수량은 숫자여야 합니다.' })
+    @Min(1,{ message: '수량은 최소 1개 이상이여야 합니다.' })
+    quantity: number;
+}
--- src/cart_items/dto/update-cart_item.dto.ts
+++ src/cart_items/dto/update-cart_item.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCartItemDto } from './create-cart_item.dto';
+import { IsInt, Min } from 'class-validator';
 
-export class UpdateCartItemDto extends PartialType(CreateCartItemDto) {}
+export class UpdateCartItemDto {
+  @IsInt({ message: '수량은 숫자여야 합니다.' })
+  @Min(1, { message: '수량은 최소 1개 이상이어야 합니다.' })
+  quantity: number;
+}
