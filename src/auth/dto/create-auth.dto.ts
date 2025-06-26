9daefb68cdfa6574d3ccef029baa724038f76693
moon <msj7191@naver.com>
2025-06-26 22:39:26 +0900

    refactor(auth) : 임포트 없는것 제거

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateAuthDto {
     @IsEmail({},{ message: "'올바은 이메일을 입력해주세요.'"})
