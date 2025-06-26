51fad75b307e17b0af360020b39a90629421522d
moon <msj7191@naver.com>
2025-06-26 19:27:04 +0900

    feature(auth): 비밀번호 해싱을 위한 비크립트 설치 및 서비스 작업

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { User } from 'src/users/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -20,20 +20,16 @@ export class AuthService {
     private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService,
   ){}
+  
+  async signup(createAuthDto: CreateAuthDto):Promise<void>{
+    const { email , password , name } =createAuthDto;
 
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+    const existing = await this.userRepository.findOne({ where : { email } });
+    if (existing) {
+      throw new ConflictException('이미 가입된 이메일입니다.');
+    }
+    const salt = await bcrypt.hash(password, salt);
+    
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
   }
 }
