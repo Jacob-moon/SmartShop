7b7dbb8014c239b613a3c97aa4e4197ad51872a0
moon <msj7191@naver.com>
2025-06-26 19:02:57 +0900

    featuer(auth): jwt 토큰 사용을 위한 @nestjs/jwt 패키지 설치 및 auth.service 구축

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { User } from 'src/users/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+
+interface JwtPayload {
+  email: string;
+  userId: number;
+}
 
 @Injectable()
 export class AuthService {
+
+  private tokenBlacklist: Set<String> = new Set();
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ){}
+
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
