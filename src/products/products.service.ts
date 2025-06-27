9eb5c7dfab9686ceb5c3ea7f4c576f0d931158d2
moon <msj7191@naver.com>
2025-06-27 21:43:36 +0900

    feature(products): product 서비스 상품 등록 추가

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
+ constructor(
+  @InjectRepository(Product)
+  private readonly productRepository: Repository<Product>
+ ){}
 
-  findAll() {
-    return `This action returns all products`;
-  }
+async createProduct(createProductDto: CreateProductDto): Promise<Product>{
+  const product = this.productRepository.create(createProductDto);
+  return await this.productRepository.save(product);
+}
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
-
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
 }
