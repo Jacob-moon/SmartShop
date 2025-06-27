e5c7299115ea845c93d696bd8867699d2eeac410
moon <msj7191@naver.com>
2025-06-27 19:51:17 +0900

    feature(users): 유저 컨트롤러 유저 상세조회  추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ForbiddenException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CurrentUser } from './user.decorator';
+
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
+  async getProfile(@Param('id') idParam: string, @CurrentUser('userId') userId: number,) {
+    const id = Number(idParam);
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    if(id !== userId) {
+      throw new ForbiddenException('접근 권한이 없습니다.')
+    }
+    
+    return this.usersService.getUserById(id);
   }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+  
 }
