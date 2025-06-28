dd0185c24763b323e43a8d1f19e8eb687d5571ad
moon <msj7191@naver.com>
2025-06-28 16:00:03 +0900

    feature(product-options): 컨트롤러 옵션 삭제 추가

--- src/product-options/product-options.controller.ts
+++ src/product-options/product-options.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ProductOptionsService } from './product-options.service';
 import { CreateProductOptionDto } from './dto/create-product-option.dto';
 import { CurrentUser } from 'src/users/user.decorator';
 import { ProductOptionResponseDto } from './dto/product-option-response.dto';
+import { UpdateProductOptionDto } from './dto/update-product-option.dto';
 
 @Controller('product-options')
 export class ProductOptionsController {
@@ -33,5 +34,25 @@ export class ProductOptionsController {
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<ProductOptionResponseDto> {
     return this.productOptionsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  async update(
+    @Param('id', ParseIntPipe) id:number,
+    @Body() updateProductOptionDto: UpdateProductOptionDto,
+    @CurrentUser('userId') userId: number,
+  ): Promise<ProductOptionResponseDto> {
+    return this.productOptionsService.update(id, updateProductOptionDto, userId);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  async remove(
+    @Param('id', ParseIntPipe) id: number,
+    @CurrentUser('userId') userId: number,
+  ): Promise<{ message: string }> {
+    await this.productOptionsService.remove(id, userId);
+    return { message: '옵션이 삭제되었습니다.' }
   }
 }
