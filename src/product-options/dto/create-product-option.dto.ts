01490901647916269b889e8f1afceb35a7cdd399
moon <msj7191@naver.com>
2025-06-28 15:30:39 +0900

    feature(product-options): 서비스 옵션 생성 ,권한 인증추가 추가 및 create dto 수정 (권한 인증을 위해)

--- src/product-options/dto/create-product-option.dto.ts
+++ src/product-options/dto/create-product-option.dto.ts
@@ -1,6 +1,10 @@
 import { IsBoolean, IsInt, IsNotEmpty, IsOptional, IsString, Min } from "class-validator";
 
 export class CreateProductOptionDto {
+    @IsNotEmpty({ message: '상품 ID를 입력해주세요' })
+    @IsInt({ message: '상품 ID는 숫자여야 합니다' })
+    productId: number;
+
     @IsNotEmpty({ message: '옵션 이름을 입력해주세요' })
     @IsString()
     name: string;
--- src/product-options/product-options.service.ts
+++ src/product-options/product-options.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { Repository } from 'typeorm';
+import { ProductOption } from './entities/product-option.entity';
 import { CreateProductOptionDto } from './dto/create-product-option.dto';
-import { UpdateProductOptionDto } from './dto/update-product-option.dto';
+import { ProductOptionResponseDto } from './dto/product-option-response.dto';
 
 @Injectable()
 export class ProductOptionsService {
-  create(createProductOptionDto: CreateProductOptionDto) {
-    return 'This action adds a new productOption';
+  constructor(
+    @InjectRepository(ProductOption)
+    private readonly optionRepository: Repository<ProductOption>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ){}
+
+  async create(
+    createProductOptionDto: CreateProductOptionDto, 
+    userId: number
+  ): Promise<ProductOptionResponseDto> {
+    const product = await this.productRepository.findOne({
+      where: { id: createProductOptionDto.productId },
+      relations: ['user'],
+    })
+    if(!product || product.user.id !== userId) {
+      throw new ForbiddenException('상품 옵션을 등록할 권한이 없습니다.')
+    }
+
+    const option = this.optionRepository.create({
+      ...createProductOptionDto,
+      product,
+    });
+    const savedOption = await this.optionRepository.save(option);
+    return new ProductOptionResponseDto(savedOption);
   }
 
-  findAll() {
-    return `This action returns all productOptions`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} productOption`;
-  }
-
-  update(id: number, updateProductOptionDto: UpdateProductOptionDto) {
-    return `This action updates a #${id} productOption`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} productOption`;
-  }
+  
 }
