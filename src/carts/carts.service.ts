6b06bfb28397976ae54b862a77dd62233c633a44
moon <msj7191@naver.com>
2025-07-15 18:55:09 +0900

    ToDo(carts): 추후 성능최적화 메모

--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -12,6 +12,12 @@ export class CartsService {
     @InjectRepository(CartItem)
     private readonly cartItemRepository: Repository<CartItem>,
   ){}
+
+  /** 
+  * ToDo: 성능최적화 => 
+  * 로직 분리 : 1.카트 있을시 userId 반환 및 카트 없을시 카드 생성 
+  *           2.카트에 담을수있도록 배열생성 및 카트 저장
+  */
 async getCartByUser(userId: number): Promise<Cart> {
   let cart = await this.cartRepository.findOne({
     where: { user: { id: userId } },
