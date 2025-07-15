6b91361c33577514480e8b1e4f92d27e7f56df9f
moon <msj7191@naver.com>
2025-07-15 18:12:16 +0900

    feature(carts): 서비스 로직 조회 + 카트생성 추후 성능최적화대상

--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from 'src/cart_items/entities/cart_item.entity';
 
 @Injectable()
 export class CartsService {
-
-  findAll() {
-    return `This action returns all carts`;
+  constructor(
+    @InjectRepository(Cart)
+    private readonly cartRepository: Repository<Cart>,
+    @InjectRepository(CartItem)
+    private readonly cartItemRepository: Repository<CartItem>,
+  ){}
+async getCartByUser(userId: number): Promise<Cart> {
+  let cart = await this.cartRepository.findOne({
+    where: { user: { id: userId } },
+    relations: ['user', 'items', 'items.productOption'],
+  });
+  if (!cart) {
+    const newCart = this.cartRepository.create({ user: { id: userId }, items: [] });
+    cart = await this.cartRepository.save(newCart);
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} cart`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} cart`;
-  }
+  return cart;
+}
 }
