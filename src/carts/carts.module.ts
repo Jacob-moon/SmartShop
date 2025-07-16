993a7af47fad4b1f83c7c0b575bfc2a4eed4a427
moon <msj7191@naver.com>
2025-07-16 17:50:08 +0900

    feature(carts): 카트 모듈 타입오알엠설정

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from 'src/cart_items/entities/cart_item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart, CartItem])],
   controllers: [CartsController],
   providers: [CartsService],
 })
