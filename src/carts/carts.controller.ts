5bfbd948410778f790252e24c413d2798b84b90d
moon <msj7191@naver.com>
2025-07-16 17:41:37 +0900

    feature(carts): 장바구니 아이템 수량수정추가

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, ParseIntPipe } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CurrentUser } from 'src/users/user.decorator';
 import { Cart } from './entities/cart.entity';
 import { CreateCartItemDto } from 'src/cart_items/dto/create-cart_item.dto';
 import { CartItem } from 'src/cart_items/entities/cart_item.entity';
+import { UpdateCartItemDto } from 'src/cart_items/dto/update-cart_item.dto';
 
 @Controller('carts')
 export class CartsController {
@@ -23,4 +24,14 @@ export class CartsController {
    return this.cartsService.addItem(userId, createCartItemDto); 
   }
 
+  @Patch('items/:id')
+  @HttpCode(200)
+  async updateItem(
+    @CurrentUser('userId') userId: number,
+    @Param('id', ParseIntPipe) itemId:number,
+    @Body() updateCartItemDto:UpdateCartItemDto,
+  ): Promise<void>{
+    return this.cartsService.updateItem(userId,itemId,updateCartItemDto)
+  }
+ 
 }
