99b74c0f9d4ed02f07db8626260346bc293d281b
moon <msj7191@naver.com>
2025-07-16 18:05:15 +0900

    feature(carts): JWT 가드 추가 우선 장바구니 회원만 추가가능 추후 비회원도 할수있도록 수정예정

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,21 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CurrentUser } from 'src/users/user.decorator';
 import { Cart } from './entities/cart.entity';
 import { CreateCartItemDto } from 'src/cart_items/dto/create-cart_item.dto';
 import { CartItem } from 'src/cart_items/entities/cart_item.entity';
 import { UpdateCartItemDto } from 'src/cart_items/dto/update-cart_item.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('carts')
 export class CartsController {
   constructor(private readonly cartsService: CartsService) {}
 
+  
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getCart(@CurrentUser('userId') userId:number):Promise<Cart>{
     return this.cartsService.getCartByUser(userId);
   }
   
   @Post('items')
+  @UseGuards(JwtAuthGuard)
   @HttpCode(201)
   async addItem(
     @CurrentUser('userId') userId: number,
@@ -23,8 +27,9 @@ export class CartsController {
   ):Promise<CartItem>{
    return this.cartsService.addItem(userId, createCartItemDto); 
   }
-
+  
   @Patch('items/:id')
+  @UseGuards(JwtAuthGuard)
   @HttpCode(200)
   async updateItem(
     @CurrentUser('userId') userId: number,
@@ -33,8 +38,9 @@ export class CartsController {
   ): Promise<void>{
     return this.cartsService.updateItem(userId,itemId,updateCartItemDto)
   }
- 
+  
   @Delete('items/:id')
+  @UseGuards(JwtAuthGuard)
   @HttpCode(200)
   async removeItem(
     @CurrentUser('userId') userId:number,
