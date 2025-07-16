d94d6f41fbb59894ec58ce72bd169df034cb1fa8
moon <msj7191@naver.com>
2025-07-16 17:38:57 +0900

    feature(carts): 컨트롤러 장바구니 아이템 추가

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CurrentUser } from 'src/users/user.decorator';
 import { Cart } from './entities/cart.entity';
+import { CreateCartItemDto } from 'src/cart_items/dto/create-cart_item.dto';
+import { CartItem } from 'src/cart_items/entities/cart_item.entity';
 
 @Controller('carts')
 export class CartsController {
@@ -11,14 +13,14 @@ export class CartsController {
   async getCart(@CurrentUser('userId') userId:number):Promise<Cart>{
     return this.cartsService.getCartByUser(userId);
   }
+  
+  @Post('items')
+  @HttpCode(201)
+  async addItem(
+    @CurrentUser('userId') userId: number,
+    @Body() createCartItemDto:CreateCartItemDto,
+  ):Promise<CartItem>{
+   return this.cartsService.addItem(userId, createCartItemDto); 
+  }
 
-//   @Get(':id')
-//   findOne(@Param('id') id: string) {
-//     return this.cartsService.findOne(+id);
-//   }
-
-//   @Delete(':id')
-//   remove(@Param('id') id: string) {
-//     return this.cartsService.remove(+id);
-//   }
-// }
+}
