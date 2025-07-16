3be2b2d7d62b937b8e490b12410b86f7427ccbee
moon <msj7191@naver.com>
2025-07-16 17:37:09 +0900

    feature(carts): 컨트롤러 전체조회 추

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,22 +1,24 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CartsService } from './carts.service';
+import { CurrentUser } from 'src/users/user.decorator';
+import { Cart } from './entities/cart.entity';
 
 @Controller('carts')
 export class CartsController {
   constructor(private readonly cartsService: CartsService) {}
 
   @Get()
-  findAll() {
-    return this.cartsService.findAll();
+  async getCart(@CurrentUser('userId') userId:number):Promise<Cart>{
+    return this.cartsService.getCartByUser(userId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cartsService.findOne(+id);
-  }
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.cartsService.findOne(+id);
+//   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.cartsService.remove(+id);
-  }
-}
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.cartsService.remove(+id);
+//   }
+// }
